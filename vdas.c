21a220ae23177fc233e8bec288112e953aaad376
xTvini <142419660+xTvini@users.noreply.github.com>
2024-08-21 16:03:53 -0300

    BCD!!

--- vdas.c
+++ vdas.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
-    int binario[32], hexal[32], octal[32],num, op;
+    int binario[32], hexal[32], octal[32], bcd[32],num, op;
     scanf("%d", &op);
     scanf("%d", &num);
+    int aux= num;
+    if(num<0){
+        num=abs(num);
+    }
     switch(op){
         case 1:
             for(int i=0;i<32;i++){
@@ -26,10 +30,13 @@ int main(void){
                 }else{
                     hexal[i]=0;
                 }
-            }    
+            }if(aux<0){
+                printf("-");
+            }   
             for(int i=31;i>=0;i--){
                 printf("%d", hexal[i]);
             }
+            break;
         case 3:
             for(int i=0;i<32;i++){
                 if(num>0){
@@ -38,9 +45,29 @@ int main(void){
                 }else{
                     octal[i]=0;
                 }
+            }if(aux<0){
+                printf("-");
             }
             for(int i=31;i>=0;i--){
                 printf("%d", octal[i]);
             }
+            break;
+        case 4:
+            int i = 0;
+            while(num>0){
+                int digitos = num %10;
+                for(int j=0; j<4; j++){
+                    bcd[i+j] = digitos%2;
+                    digitos/=2;
+                }
+                i+=4;
+                num/=10;
+            }if(aux<0){
+                printf("-");
+            }
+            for( int k = i -1; k>=0; k--){
+                printf("%d", bcd[k]);
+            }
+            break;
     }
 }
