18edc40370062316c1eaf2bf73b73e0d74e04a82
xTvini <142419660+xTvini@users.noreply.github.com>
2024-08-25 17:49:49 -0300

    decimal pra float e pra double

--- vdas.c
+++ vdas.c
@@ -11,6 +11,7 @@ Mudanças:
 4- 21/08/24 16:04 - BCD, e correção de falhas no codigo;
 5- 24/08/24 18:26 - Correção do codigo;
 6- 25/08/24 12:15 - Complemento a 2;
+7- 25/08/24 17:47 - decimal pra float e pra double;
 
 Instruções: 
 
@@ -21,10 +22,11 @@ b) base 8,✅ caso 2
 c) base16, ✅ caso 3 
 d) código BCD✅ caso 4
 
-2 - de base 10  para base com sinal com 16 bits, (complemento a 2)✅
-3 - converter real em decimal para float e double, mostrando os respectivos bits de sinal , expoente, expoente com viés e fração 
+2 - de base 10  para base com sinal com 16 bits, (complemento a 2)✅ caso 5
+3 - converter real em decimal para float e double, mostrando os respectivos bits de sinal , expoente, expoente com viés e fração ✅
+
 OBS: deve ser entregue o executável com as iniciais do email e link do git com código.  
-A cada questão implementada deve ser feito um comite no git . 
+A cada questão implementada deve ser feito um commit no git . 
 O codigo fonte deve conter um cabeçalho dos dados do autor e  revisões/ atualizações com datas e hora
 
 */
@@ -33,101 +35,136 @@ O codigo fonte deve conter um cabeçalho dos dados do autor e  revisões/ atuali
 
 int main(void) {
     int binario[32], hexal[32], octal[32], bcd[32], num, op;
+    unsigned int complemento;
+    unsigned int mascara;
+    int i, j;
+
     scanf("%d", &op);
-    scanf("%d", &num);
-    int aux = num;
-    int i = 0;
+
     switch (op) {
         case 1:
+            scanf("%d", &num);
             if (num < 0) {
                 num = abs(num);
             }
-            for (i = 0; num > 0; i++) {
-                binario[i] = num % 2;
-                num = num / 2;
+            i = 0;
+            while (num > 0) {
+                binario[i++] = num % 2;
+                num /= 2;
             }
-            if (aux < 0) {
-                printf("-");
-            }
-            for (int j = i - 1; j >= 0; j--) {
+            for (j = i - 1; j >= 0; j--) {
                 printf("%d", binario[j]);
             }
             break;
 
         case 2:
+            scanf("%d", &num);
             if (num < 0) {
                 num = abs(num);
             }
-            for (i = 0; num > 0; i++) {
-                hexal[i] = num % 16;
-                num = num / 16;
+            i = 0;
+            while (num > 0) {
+                hexal[i++] = num % 16;
+                num /= 16;
             }
-            if (aux < 0) {
-                printf("-");
-            }
-            for (int j = i - 1; j >= 0; j--) {
-                printf("%d", hexal[j]);
+            for (j = i - 1; j >= 0; j--) {
+                printf("%X", hexal[j]);
             }
             break;
 
         case 3:
+            scanf("%d", &num);
             if (num < 0) {
                 num = abs(num);
             }
-            for (i = 0; num > 0; i++) {
-                octal[i] = num % 8;
-                num = num / 8;
+            i = 0;
+            while (num > 0) {
+                octal[i++] = num % 8;
+                num /= 8;
             }
-            if (aux < 0) {
-                printf("-");
-            }
-            for (int j = i - 1; j >= 0; j--) {
+            for (j = i - 1; j >= 0; j--) {
                 printf("%d", octal[j]);
             }
             break;
 
         case 4:
+            scanf("%d", &num);
             if (num < 0) {
                 num = abs(num);
             }
-            for (i = 0; num > 0; i += 4) {
+            i = 0;
+            while (num > 0) {
                 int digitos = num % 10;
-                for (int j = 0; j < 4; j++) {
+                for (j = 0; j < 4; j++) {
                     bcd[i + j] = digitos % 2;
                     digitos /= 2;
                 }
                 num /= 10;
+                i += 4;
             }
-            if (aux < 0) {
-                printf("-");
-            }
-            for (int k = i - 1; k >= 0; k--) {
-                printf("%d ", bcd[k]);
+            for (j = i - 1; j >= 0; j--) {
+                printf("%d ", bcd[j]);
             }
             break;
+
         case 5:
-            unsigned int complemento;
-            unsigned int mascara = 1 << 15;
-            if (num < -32768 || num > 32767) {
-                return 1;
-            }
-            if (num < 0) {
-                complemento = (unsigned int)(num & 0xFFFF);
-            } else {
-                complemento = (unsigned int)num;
-            }
-            for (int i = 0; i<16; i++) {
-                if (complemento & mascara) {
-                    printf("1");
-                } else {
-                    printf("0");
-                }
+            scanf("%d", &num);
+            complemento = (num < 0) ? (~(-num) + 1) & 0xFFFF : (unsigned int)num;
+            mascara = 1 << 15;
+            for (i = 0; i < 16; i++) {
+                printf("%c", (complemento & mascara) ? '1' : '0');
                 mascara >>= 1;
             }
-                    break;
-                default:
-                    printf("Opção inválida!\n");
+            break;
+
+        case 6: {
+            float floatNum;
+            union {
+                float f;
+                unsigned int bits;
+            } floatUnion;
+
+            scanf("%f", &floatNum);
+            floatUnion.f = floatNum;
+
+            unsigned int sinal = (floatUnion.bits >> 31) & 1;
+            unsigned int expoente = (floatUnion.bits >> 23) & 0xFF;
+            unsigned int fracao = floatUnion.bits & 0x7FFFFF;
+
+            printf("Float: %f\n", floatNum);
+            printf("Sinal: %u\n", sinal);
+            printf("Expoente: %u\n", expoente);
+            printf("Expoente com viés: %d\n", expoente - 127);
+            printf("Fração: %u\n", fracao);
+            break;
+        }
+
+        case 7: {
+            double doubleNum;
+            union {
+                double d;
+                unsigned long long bits;
+            } doubleUnion;
+
+            scanf("%lf", &doubleNum);
+            doubleUnion.d = doubleNum;
+
+            unsigned long long sinal = (doubleUnion.bits >> 63) & 1;
+            unsigned long long expoente = (doubleUnion.bits >> 52) & 0x7FF;
+            unsigned long long fracao = doubleUnion.bits & 0xFFFFFFFFFFFFF;
+
+            printf("Double: %lf\n", doubleNum);
+            printf("Sinal: %llu\n", sinal);
+            printf("Expoente: %llu\n", expoente);
+            printf("Expoente com viés: %lld\n", expoente - 1023);
+            printf("Fração: %llu\n", fracao);
+            break;
+        }
+
+        default:
+            printf("Opção inválida!\n");
             break;
     }
+
     return 0;
 }
